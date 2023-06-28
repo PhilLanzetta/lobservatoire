2722867549499331e047c70c968d7ad3e90d42b4
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2023-06-28 10:43:02 -0400

    initial commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -39,9 +39,9 @@ const Layout = ({ children }) => {
             fontSize: `var(--font-sm)`,
           }}
         >
-          © {new Date().getFullYear()} &middot; Built with
+          © {new Date().getFullYear()} &middot; Website Designed and Developed by
           {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          <a href="https://www.pacificpacific.pub">Pacific</a>
         </footer>
       </div>
     </>
