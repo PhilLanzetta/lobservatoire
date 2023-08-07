351396ad975bcb1fbc0f17c69613433e9e79e249
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2023-08-07 11:46:28 -0400

    adding pages

---
+++ src/pages/team.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const Team = () => {
+  return (
+    <Layout>
+      <div>Team</div>
+    </Layout>
+  )
+}
+
+export default Team
